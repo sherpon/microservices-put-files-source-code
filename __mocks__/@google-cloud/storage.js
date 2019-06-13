d8729725588e987054cfdc81d23fc7acf81a09a5
bruce grover lee <brucegrover.lee@gmail.com>
2019-06-12 19:12:20 -0500

    first commit

---
+++ __mocks__/@google-cloud/storage.js
@@ -0,0 +1,27 @@
+/**
+ * https://jestjs.io/docs/en/manual-mocks
+ */
+
+const { Storage } = jest.genMockFromModule('@google-cloud/storage');
+
+class mockStorage extends Storage {
+  constructor(opt) {
+    super(opt);
+  }
+
+  bucket() {
+    return {
+      file: function () {
+        return {
+          copy: function () {
+            return new Promise((resolve, reject) => {
+              resolve(true);
+            });
+          }
+        };
+      },
+    };
+  }
+};
+
+exports.Storage = mockStorage;
